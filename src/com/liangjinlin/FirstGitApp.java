c7323ecf20105fcfba952ed6bb132d5f536814f8
ljlxhbc <ljlxhbc@163.com>
2020-04-23 15:50:16 +0800

    同一行网页端测试
    tonghang

--- src/com/liangjinlin/FirstGitApp.java
+++ src/com/liangjinlin/FirstGitApp.java
@@ -5,7 +5,7 @@ package com.liangjinlin;
  */
 public class FirstGitApp {
     public static void main(String[] args) {
-        System.out.println("manager first commit");
+        System.out.println("manager first commit 网页端");
         //这就是网页端的修改，同时也是代码的修改
     }
 }
