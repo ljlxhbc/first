70789b1ce9f30609541f295b357eb07867fed7c8
ljlxhbc <ljlxhbc@163.com>
2020-04-23 15:36:11 +0800

    代码段修改

--- src/com/liangjinlin/FirstGitApp.java
+++ src/com/liangjinlin/FirstGitApp.java
@@ -6,6 +6,6 @@ package com.liangjinlin;
 public class FirstGitApp {
     public static void main(String[] args) {
         System.out.println("manager first commit");
-        //这就是网页端的修改
+        //这就是网页端的修改，同时也是代码的修改
     }
 }
